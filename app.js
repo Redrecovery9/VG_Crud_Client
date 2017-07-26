6233137f84093a6d68ddc8bbdf3d0d4f95261977
Jonathan Sax <jsax8310@outlook.com>
2017-07-25 20:44:42 -0600

    added modal

--- app.js
+++ app.js
@@ -13,10 +13,24 @@ $(document).ready(function() {
       $('.table_body').append(
         `<tr><td>${name}</td><td>${platform}</td><td>${beaten}</td><td>${rating}</td>
         <td><button type="button" class="btn btn-default">👾</button></td>
-        <td><button type="button" class="btn btn-default">Edit
+        <td><button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal">
+          Edit
         <span class="glyphicon glyphicon-wrench"></span></button></td>
         <td><button type="button" class="btn btn-default">X</button></td></tr>`
       )
     }
+    $('.modal-body').append(
+      `<form>
+        <div class="btn-group" data-toggle="buttons">
+          <h5>Have you beaten<br>the game?</h5>
+          <label class="btn btn-default active">
+            <input type="radio" name="options" id="option1" autocomplete="off" checked> False
+          </label>
+          <label class="btn btn-default">
+            <input type="radio" name="options" id="option2" autocomplete="off"> True
+          </label>
+        </div>
+      </form>`
+    )
   })
 })
